6aff5899bf6eb5c791cc95367055f4ca0eec48fe
saramonteiromartins <sara.monteiro.martins@uniklinik-freiburg.de>
2025-10-15 10:20:42 +0200

    cleaning code

--- 00_scripts/01_preprocessing.R
+++ 00_scripts/01_preprocessing.R
@@ -58,8 +58,6 @@ sumstats_filt <- regions_list$sumstats_filt
 writeLines(regions_log, con = paste0(output_path, "_get_coloc_regions_log.txt"))
 saveRDS(sumstats_filt, paste0(output_path, "_subset.RDS"))
 
-#Following code substitute:
-#save_coloc_regions(regions_list, output_path, sumstats_1_type=sumstats_type)
 write.table(regions, paste0(output_path, "_coloc_regions_PASS.tsv"),
             sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
 
@@ -122,19 +120,12 @@ ld=read.table(paste0(output_path, "_ld_results.ld"),header=T)
 r2.cutoff <- r2_thresh 
 
 ld_results=ld[which(ld$R2>r2.cutoff),]
-#sumstats_filt=readRDS(paste0(output_path, "_subset.RDS"))
 sumstats_filt=as.data.frame(sumstats_filt)
-#ld_results=merge(ld_results, sumstats_filt[,c("rsID", "A1","A2")], by.x="SNP_A", by.y="rsID")
+
 #I want the ref and alt from the proxies! if the r2==1 proxie and lead is the same snp
 ld_results=merge(ld_results, sumstats_filt[,c("rsID", "A1","A2")], by.x="SNP_B", by.y="rsID")
 
 ld_results$strand <- "+"
-
-#ld_results$ref <- sapply(ld_results$SNP_B, function(x) unlist(strsplit(x, ":"))[3])
-#ld_results$alt <- sapply(ld_results$SNP_B, function(x) unlist(strsplit(x, ":"))[4])
-#ld_results$allele <- paste(ld_results$ref, ld_results$alt, sep = "/") #A2 is reference
-#ld_results$END =as.numeric(ld_results$BP_B) +  nchar(as.character(ld_results$ref)) - 1 #need to addapt to indels
-
 
 ld_results$allele <- paste(ld_results$A2, ld_results$A1, sep = "/") #A2 is reference
 ld_results$END =as.numeric(ld_results$BP_B) +  nchar(as.character(ld_results$A2)) - 1 #need to addapt to indels
@@ -158,16 +149,7 @@ write.table(proxy_data, file= paste0(output_path, "_proxies.txt"), quote = FALSE
     row.names = FALSE, col.names = TRUE)
 
 
-#FOR VEP ANALYSIS NEED TO CHECK ALLELE POSITION BASED ON dbSNP - use
-#Name_by_position(sumstats, tmp_name=NULL,CHR_name="CHR_hg38", POS_name="POS_hg38",A1_name="A1_hg38", A2_name="A2_hg38",Name_out="Name_hg38", rs_name="rs",unique_ID_name="unique_ID",tabix_bin, dbSNP_file,do_soring=T, mc_cores=4)
-
-#devtools::load_all("/data/programs/pipelines/genepicoloc/genepicoloc_package")
-#library(data.table)
-#setDT(ld_results)
-#ld_results2 <- Name_by_position(ld_results, tmp_name=NULL,CHR_name="CHR_B", POS_name="BP_B",A1_name="alt", A2_name="ref",Name_out="Name_new",unique_ID_name="unique_ID",tabix_bin="tabix", dbSNP_file="/data/public_resources/Ensembl_human_variation_b38_v109/dbSNP_v156_b38p14_rsid.vcf.gz",do_soring=T)
-#ld_results2 <- separate(ld_results2, Name_new, into = c("chr_new", "pos_new", "ref_new", "alt_new"), sep = ":")
-
-#ld_results2$allele <- paste(ld_results2$ref, ld_results2$alt, sep = "/") #A2 is reference
+#FOR VEP ANALYSIS NEED TO CHECK ALLELE POSITION BASED ON dbSNP
 
 vep_data <- ld_results[,c("CHR_B", "BP_B", "END", "allele", "strand", "SNP_B")]
 ##!VEP used "X" instead of "23"
@@ -180,5 +162,4 @@ print(head(vep_data))
 write.table(vep_data, file = paste0(output_path, "_proxies_vep.txt"), quote = FALSE, sep = "\t",
         row.names = FALSE, col.names = FALSE)
 
-
-#cat("\n## Pre-processing finished ##\n")
+cat("\n## Pre-processing finished ##\n")
